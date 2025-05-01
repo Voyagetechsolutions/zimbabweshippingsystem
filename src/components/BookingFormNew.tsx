c98f74f8ec9234620ec1413403f8785e2e576a3a
Voyagetechsolutions <voyagetechsolutions@gmail.com>
2025-05-01 19:54:35 +0000

    Code edited in Lovable Code Editor
    Edited UI in Lovable

--- src/components/BookingFormNew.tsx
+++ src/components/BookingFormNew.tsx
@@ -1347,7 +1347,7 @@ const BookingFormNew: React.FC<BookingFormProps> = ({ onSubmitComplete }) => {
                 onComplete={handlePaymentComplete}
               />
               
-              <div className="mt-6 border-t pt-6">
+              {/*<div className="mt-6 border-t pt-6">
                 <FormField
                   control={form.control}
                   name="terms"
@@ -1371,7 +1371,7 @@ const BookingFormNew: React.FC<BookingFormProps> = ({ onSubmitComplete }) => {
                     </FormItem>
                   )}
                 />
-              </div>
+              </div>*/}
               
               <div className="flex justify-between mt-6">
                 <Button 
